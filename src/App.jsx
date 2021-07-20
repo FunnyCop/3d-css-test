cdea7f900bbbe2fa320d67245234c3d1aae7f537
Logan Prunty <lb_prunty@outlook.com>
2021-07-20 13:22:02 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import "./static/css/App.css"
+
+const App = () => {
+  return (
+    <div id = "App">
+      <div id = "Scene">
+        <div id = "Text">
+          <p>mayonnaise is not an instrument</p>
+        </div>
+
+        <div id = "Floor"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
